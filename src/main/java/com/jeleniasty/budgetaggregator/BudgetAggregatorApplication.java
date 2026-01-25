0191b2958c52a8445b28bdca89184b610e0514ff
micha <michal.jeleniasty@gmail.com>
2026-01-25 10:32:18 +0100

    Created empty project

---
+++ src/main/java/com/jeleniasty/budgetaggregator/BudgetAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.jeleniasty.budgetaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetAggregatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeleniasty/budgetaggregator/BudgetAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeleniasty.budgetaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
